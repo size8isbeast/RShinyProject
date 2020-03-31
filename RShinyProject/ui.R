d2e8c1da65445f69a4ffde7e712f74943cfb317d
jeremyjiang3 <44796470+jeremyjiang3@users.noreply.github.com>
2020-03-30 17:08:17 -0700

    draft

--- RShinyProject/ui.R
+++ RShinyProject/ui.R
@@ -1,33 +1,41 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+shinyUI(fluidPage(
+    navbarPage(
+        
+        "007 Movie Series Performance",
+        tabPanel("Movie Comparsion ",
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
+            h4("Filter"),
+            
+            sliderInput("year", "Year released", 1940, 2014, value = c(1970, 2014),
+                        sep = ""),
+            sliderInput("budget", "Budget", 1, 100, 30),
+            sliderInput("boxoffice", "Dollars at Box Office (millions)",
+                        0, 800, c(0, 800), step = 1),
+            actionButton("button", "An action button"),
+            textInput(" text","Gnere", value="Action")),
+       
         mainPanel(
-            plotOutput("distPlot")
+            fluidPage(fluidRow(
+                column(6,
+                       plotOutput(
+                           "glm", width = "700px", height = "600px"
+                       )),
+                column(6,
+                       DT::dataTableOutput("dataSet"))
+              
+            ))
+                         
+            )
         )
-    )
-))
+    ),
+    tabPanel("James Bond Actors", "This panel is intentionally left blank"),
+    tabPanel("Behind the 007", "This panel is intentionally left blank")
+        )
+    ))

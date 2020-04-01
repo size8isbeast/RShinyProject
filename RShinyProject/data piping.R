d3307e7b3a16b3fd0f9139bc08908e1e71c68d74
JeremyJ <jeremyjiangjs@gmail.com>
2020-04-01 11:24:35 -0700

    c

--- RShinyProject/data piping.R
+++ RShinyProject/data piping.R
@@ -17,11 +17,11 @@ library(recommenderlab)
 
 
 #read csvs
-movies <- read_csv("/www/data/tmdb_5000_movies.csv", na="NA")
-credits <- read_csv("/www/data/tmdb_5000_credits.csv",  na="NA")
-oscars <- read_csv("/www/data/oscar.csv",  na="NA")
-Coolcars <- read_csv("/www/data/cars.csv")
-Kills <- read_csv("/www/data/Kills per Bond movie.csv",  na="NA")
+movies <- read_csv("www/data/tmdb_5000_movies.csv", na="NA")
+credits <- read_csv("www/data/tmdb_5000_credits.csv",  na="NA")
+oscars <- read_csv("www/data/oscar.csv",  na="NA")
+Coolcars <- read_csv("www/data/cars.csv")
+Kills <- read_csv("www/data/Kills per Bond movie.csv",  na="NA")
 
 
 #loading data

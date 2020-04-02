32b838a2a941367609e33a41070fa4f4ee644164
JeremyJ <jeremyjiangjs@gmail.com>
2020-04-01 19:07:32 -0700

    server basic

--- RShinyProject/server.R
+++ RShinyProject/server.R
@@ -12,7 +12,7 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$plot <- renderPlot({
 
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
@@ -20,7 +20,82 @@ shinyServer(function(input, output) {
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        
+        
+    
 
     })
+    output$dataSet <- DT::renderDataTable({
+        DT::datatable(cars)
+    })
+    
+    output$graph<- renderPlot({
+        
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        
+        
+        
+        
+    })
+    
+    output$actor <- DT::renderDataTable({
+        DT::datatable(actor)
+    })
+    
+    output$myImage <- renderImage({
+        if (is.null(input$picture))
+            return(NULL)
+        
+        if (input$picture == "face") {
+            return(list(
+                src = "images/face.png",
+                contentType = "image/png",
+                alt = "Face"
+            ))
+        } else if (input$picture == "chainring") {
+            return(list(
+                src = "images/chainring.jpg",
+                filetype = "image/jpeg",
+                alt = "This is a chainring"
+            ))
+        }
+        
+    }, deleteFile = FALSE)
+
+    output$car<- renderPlot({
+        
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        
+        
+        
+        
+    })
+    
+    output$kill<- renderPlot({
+        
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        
+        
+        
+        
+    })
+    
+    
+    
 
 })
